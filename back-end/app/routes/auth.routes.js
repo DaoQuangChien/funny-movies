6be3e4e07e7272b170776513c56085b1deb3b859
ChienDao <quangchienspkt@gmail.com>
2021-03-26 19:45:28 +0700

    Added back-end services

---
+++ back-end/app/routes/auth.routes.js
@@ -0,0 +1,18 @@
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app) {
+  app.use(function (_, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  app.post(
+    "/api/auth/signup",
+    [verifySignUp.checkDuplicateEmail],
+    controller.signup
+  );
+  app.post("/api/auth/signin", controller.signin);
+};
